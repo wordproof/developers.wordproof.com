be9d30edd3183c43dc71f8a5ee18861a107c2eae
Marijn Bent <marijn@wordproof.com>
2021-06-21 17:19:18 +0200

    change uikit to ui kit

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,15 +3,15 @@ module.exports = {
     description: 'Everything you need to know about developing for WordProof',
     themeConfig: {
         sidebar: [
-            ['/uikit/', 'UIKit'],
             ['/rest-api/', 'API'],
             ['/schema/', 'Schema'],
+            ['/ui-kit/', 'UI Kit'],
             ['/plug-and-play/', 'Plug and Play'],
         ],
         nav: [
 
             { text: '🎓 Academy', link: 'https://wordproof.com/academy' },
-            { text: '💅🏻 UIKit', link: 'https://uikit.wordproof.com' },
+            { text: '💅🏻 UI Kit', link: 'https://uikit.wordproof.com' },
         ],
     },
     head: [
